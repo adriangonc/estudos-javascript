c0d320aff9d5c04b4243d15a64eb934e7df618e1
Adriano Gonçalves <adriangonc@hotmail.com>
2019-09-11 19:46:06 -0300

    Quase tudo e função em JS :D

---
+++ fundamentos/functions.js
@@ -0,0 +1,11 @@
+//Funções
+console.log(typeof Object) //Objeto em js e uma função
+
+class Produto {}
+console.log(typeof Produto) //Também e função :D
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+console.log(soma(1500, 32))
